c337f6691f258490239028902e51b5114c31c55c
maxkleshchyonok <56303675+maxkleshchyonok@users.noreply.github.com>
2023-03-10 18:40:04 +0300

    feat: add canvas

--- src/script.js
+++ src/script.js
@@ -171,3 +171,42 @@ let copy = (textId) => {
         customAlert.style.transform = "scale(0)";
     }, 2000);
 };
+
+// const imageContainer = document.querySelector('.image');
+// imageContainer.addEventListener('click' , (event) => {
+//     const color = window.getComputedStyle(event.target).backgroundColor;
+//     console.log('This is my test', color);
+// });
+
+const img = new Image();
+img.crossOrigin = "anonymous";
+img.src = "https://www.shutterstock.com/image-photo/mountains-under-mist-morning-amazing-260nw-1725825019.jpg";
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
+img.addEventListener("load", () => {
+    ctx.drawImage(img, 0, 0);
+    img.style.display = "none";
+});
+const hoveredColor = document.getElementById("hovered-color");
+const selectedColor = document.getElementById("selected-color");
+
+function pick(event, destination) {
+    const bounding = canvas.getBoundingClientRect();
+    const x = event.clientX - bounding.left;
+    const y = event.clientY - bounding.top;
+    const pixel = ctx.getImageData(x, y, 1, 1);
+    const data = pixel.data;
+
+    const rgba = `rgba(${data[0]}, ${data[1]}, ${data[2]}, ${data[3] / 255})`;
+    destination.style.background = rgba;
+    destination.textContent = rgba;
+
+    return rgba;
+}
+
+// canvas.addEventListener("mousemove", (event) => pick(event, hoveredColor));
+// canvas.addEventListener("click", (event) => pick(event, selectedColor));
+
+canvas.addEventListener("touchmove", (event) => pick(event, hoveredColor));
+canvas.addEventListener("touch", (event) => pick(event, selectedColor));
+
